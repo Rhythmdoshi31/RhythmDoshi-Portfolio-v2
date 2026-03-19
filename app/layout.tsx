880edf03f368d2352cb7d779cd617ef3693817e1
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2026-03-19 12:39:25 +0530

    fixed twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,10 @@ import Navbar from "./components/layout/Navbar";
 import ThemeToggle from "./components/ThemeToggle";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://rhythmdoshi.site'),
+  metadataBase: new URL("https://rhythmdoshi.site"),
   title: "Rhythm Doshi — Full Stack Developer portfolio",
-  description: "Full Stack Engineer building modern web applications. Explore my projects, stack, and experience in React, Next.js, Node.js and more.",
+  description:
+    "Full Stack Engineer building modern web applications. Explore my projects, stack, and experience in React, Next.js, Node.js and more.",
 
   icons: {
     icon: [
@@ -22,15 +23,29 @@ export const metadata: Metadata = {
         color: "#000000",
       },
       { rel: "msapplication-TileImage", url: "/meta/mstile144.png" },
-      ],
-    },
-    openGraph: {
-      images: [{ url: "/meta/og-image.png", width: 1200, height: 630 }],
-    },
-    twitter: {
-      images: ["/meta/twitter-image.png"],
-    },
-    manifest: "/meta/site.webmanifest",
+    ],
+  },
+  openGraph: {
+    title: "Rhythm Doshi — I Build Things for the Web",
+    description: "Full Stack Engineer building modern web applications.",
+    url: "https://rhythmdoshi.site",
+    siteName: "Rhythm Doshi",
+    images: [
+      {
+        url: "https://rhythmdoshi.site/meta/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+  card: 'summary_large_image',
+  title: "Rhythm Doshi — I Build Things for the Web",
+  description: "Full Stack Engineer building modern web applications. Explore my projects, stack, and experience in React, Next.js, Node.js and more.",
+  images: ['https://rhythmdoshi.site/meta/twitter-image.png'], // absolute URL
+},
+  manifest: "/meta/site.webmanifest",
 };
 
 export default function RootLayout({
