bfbcd1f74e1e74bb6737130f2e1660557da1639c
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2026-03-28 16:41:02 +0530

    Added permanent redirect for www in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.rhythmdoshi.site",
+          },
+        ],
+        destination: "https://rhythmdoshi.site/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
