5a75e1ae45d13990fe734e435b6c207bf22f4e86
Rhythmdoshi31 <rhythmdoshi04@gmail.com>
2026-04-05 19:00:45 +0530

    Added dark mode logo using observer

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -8,8 +8,21 @@ import { useEffect, useState } from "react";
 export default function Navbar() {
   const pathname = usePathname();
   const [hash, setHash] = useState("");
+  const [isDark, setIsDark] = useState(false);
 
   useEffect(() => {
+    const checkDark = () => {
+      setIsDark(document.documentElement.classList.contains("dark"));
+    };
+
+    checkDark();
+
+    // optional: watch for changes if you toggle theme dynamically
+    const observer = new MutationObserver(checkDark);
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
     const update = () => setHash(window.location.hash);
 
     update(); // initial
@@ -20,41 +33,55 @@ export default function Navbar() {
     return () => {
       window.removeEventListener("hashchange", update);
       window.removeEventListener("popstate", update);
+      observer.disconnect();
     };
   }, []);
 
-  const isHome = pathname === "/" && (hash !== "#projects" && hash !== "#blogs");
+  const isHome = pathname === "/" && hash !== "#projects" && hash !== "#blogs";
   const isProjects = pathname.startsWith("/project") || hash === "#projects";
   const isBlogs = hash === "#blogs";
 
   return (
     <nav className="sticky top-2 z-50 h-12 w-full flex justify-center items-center bg1 border-y">
-      
       <div className="h-full max-w-3xl w-[95%] md:full flex justify-between items-center px-8 border-x">
-        
-        <Image src="/images/newlogo.webp" alt="Logo" width={32} height={32} className="dark:bg-white rounded-md"/>
+        <Image
+      src={isDark ? "/images/newlogodark.webp" : "/images/newlogo.webp"}
+      alt="Logo"
+      width={32}
+      height={32}
+      className="rounded-full"
+    />
 
         <div className="flex gap-8">
-          
-          <Link href="/" onClick={() => setHash("")} className="hidden sm:block">
+          <Link
+            href="/"
+            onClick={() => setHash("")}
+            className="hidden sm:block"
+          >
             <h5 className={`${isHome ? "text1" : "text2 hover:text1"}`}>
               Home
             </h5>
           </Link>
 
-          <Link href="/#projects" onClick={() => setHash("#projects")} className="hidden sm:block">
+          <Link
+            href="/#projects"
+            onClick={() => setHash("#projects")}
+            className="hidden sm:block"
+          >
             <h5 className={`${isProjects ? "text1" : "text2 hover:text1"}`}>
               Projects
             </h5>
           </Link>
-          <Link href="/#blogs" onClick={() => setHash("#blogs")} className="hidden md:block">
+          <Link
+            href="/#blogs"
+            onClick={() => setHash("#blogs")}
+            className="hidden md:block"
+          >
             <h5 className={`${isBlogs ? "text1" : "text2 hover:text1"}`}>
               Blog
             </h5>
           </Link>
-
         </div>
-
       </div>
     </nav>
   );
